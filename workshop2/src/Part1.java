d80963080d2d615c0fc3b4116ba7fc369af59caa
Vandthe172465 <134364717+Vandthe172465@users.noreply.github.com>
2023-05-28 11:37:45 +0700

    Add files via upload

---
+++ workshop2/src/Part1.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.Scanner;
+/**
+ *
+ * @author Admin
+ */
+public class Part1 {
+    public static void main(String[] args) {
+        boolean cont=false;
+        do{
+            try{
+                int num;
+                Scanner sc = new Scanner(System.in);
+                System.out.println("enter the number:");
+                num=sc.nextInt();
+                if(num<1)
+                    throw new Exception();
+                System.out.println("The number is "+ num);
+                cont=false;
+            }catch(Exception e){
+                System.out.println("The number is invalid");
+                cont=true;
+            }
+        }
+        while(cont);
+    }
+}
