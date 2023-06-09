c891dd084a6ed6aef8ab7f7f4f42c5bffa74782d
Vandthe172465 <134364717+Vandthe172465@users.noreply.github.com>
2023-06-09 14:43:49 +0700

    Add files via upload

---
+++ workshop4/ItemManager/src/DTO/Vase.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DTO;
+import java.util.*;
+/**
+ *
+ * @author Admin
+ */
+public class Vase extends Item {
+    public int height;
+    public String material; 
+    public Vase(){
+        super();
+    }
+    public Vase(int value, String creater, int height, String material){
+        super(value, creater);
+        this.height = height;
+        this.material = material;
+    }
+    public int getHeight(){
+        return this.height;
+    }
+    public String getMaterial(){
+        return this.material;
+    }
+    public void setHeight(int height){
+        this.height = height;
+    }
+    public void setMaterial(String material){
+        this.material = material;
+    }
+    public void inputVase(){ 
+        super.input();
+        try {
+            Scanner scanner = new Scanner(System.in);
+            do {
+                System.out.print("Enter weight: ");
+                this.height = Integer.parseInt(scanner.nextLine()); 
+                System.out.print("Enter material: ");
+                this.material = scanner.nextLine();
+            } while (height < 0 || material == null);
+        } catch (NumberFormatException e) {
+            System.out.println("Incorrect information format, please enter again!");
+        }
+    }
+    public void outputVase(){
+        super.output();
+        System.out.println("Height: " + this.height);
+        System.out.println("Material: " + this.material);
+    }
+}
---
+++ workshop4/ItemManager/src/GUI/AntiqueShop.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+import DTO.Item;
+import DTO.Painting;
+import DTO.Statue;
+import DTO.Vase;
+/**
+ *
+ * @author Admin
+ */
+public class AntiqueShop {
+    public static void main(String[] args) {
+        String[] options = {"Create a Vase", "Create a Statue", "Create a Painting", "Display the Item"};
+        Item item = null;
+        int choice = 0;
+        do { 
+            choice = Menu.getChoice(options);
+            switch(choice){
+                case 1:
+                    item = new Vase();
+                    ((Vase)item).inputVase();
+                    break;
+                case 2:
+                    item = new Statue();
+                    ((Statue)item).inputStatue();
+                    break;
+                case 3:
+                    item = new Painting();
+                    ((Painting)item).inputPainting();
+                    break;
+                case 4:
+                    if(item != null){
+                        if(item instanceof Vase){
+                            ((Vase)item).outputVase();
+                        }
+                        else if(item instanceof Statue){
+                            ((Statue)item).outputStatue();
+                        }
+                        else if(item instanceof Painting){
+                            ((Painting)item).outputPainting();
+                        }
+                    }
+                    break;
+                default:
+                    break;
+            }
+        } while (choice > 0 && choice < 5);
+    }
+}
